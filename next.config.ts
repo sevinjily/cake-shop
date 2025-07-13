58db6d96a780cb3f55ce239e63aeefd1a5b1c186
svncmsva <sevincmusaeva01@gmail.com>
2025-07-13 17:05:14 +0400

    head changed

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 const nextConfig = {
   /* config options here */
-    content: [
-    './app/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './pages/**/*.{ts,tsx}', // əgər pages-dirə əsaslanırsansa
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        dancing: ['Dancing Script', 'cursive'],
-        quicksand: ['Quicksand', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
+  
+  
 
 };
 
