f64543478b7eaedbc76a1052c58f8b572c1580f2
svncmsva <sevincmusaeva01@gmail.com>
2025-07-14 16:07:53 +0400

    tailwind problems

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",],
+  theme: {
+    extend: { 
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },},
+  },
+  plugins: [],
+}
+
